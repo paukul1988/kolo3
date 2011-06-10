26b13683e3ac3cd35c84d8bca6647c0ecd6ce437
paukul1988 <paulinarak1988@gmail.com>
2011-06-10 11:35:25 +0200

    dodano plik ignorowany

--- kolo3.cpp
+++ kolo3.cpp
@@ -1,9 +1,11 @@
 //Paulina Kulaga, ABiBD, 101564
 #include <iostream>
+#include<fstream>
 using namespace std;
 
 int main(int argc, char **argv) {
   cout<<"Paulina Kulaga, ABiBD, 101564"<<endl;
+  ifstream("info-student.txt");
   return 0;
 }
 
