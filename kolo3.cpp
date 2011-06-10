809bb43b62b07bbc60e42334a59d401a2f0400a4
paukul1988 <paulinarak1988@gmail.com>
2011-06-10 11:26:25 +0200

    Dodano informacje o studencie

--- kolo3.cpp
+++ kolo3.cpp
@@ -1,10 +1,9 @@
-// Imie nazwisko, grupa(specjalnosc), nr. indeksu
-
+//Paulina Kulaga, ABiBD, 101564
 #include <iostream>
 using namespace std;
 
 int main(int argc, char **argv) {
-  cout<<"Program pobiera informacje od studenta i zapisuje je do pliku"<<endl;
+  cout<<"Paulina Kulaga, ABiBD, 101564"<<endl;
   return 0;
 }
 
